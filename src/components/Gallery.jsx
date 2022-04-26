082643f80b3628e7cf73e13f0e8fa2e137e1b09e
praise159praise <praisebamiyo159@gmail.com>
2022-04-26 05:21:55 +0100

    added pictures to gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -11,6 +11,13 @@ import img8 from '../images/gallery/8.png'
 import img9 from '../images/gallery/9.jpg'
 import img10 from '../images/gallery/10.jpg'
 import img11 from '../images/gallery/11.jpg'
+import img12 from '../images/gallery/12.jpeg'
+import img13 from '../images/gallery/13.jpeg'
+import img14 from '../images/gallery/14.jpeg'
+import img15 from '../images/gallery/15.jpeg'
+import img16 from '../images/gallery/16.jpeg'
+import img17 from '../images/gallery/17.jpeg'
+import img18 from '../images/gallery/18.jpeg'
 import '../style/gallery.css'
 
 import "slick-carousel/slick/slick.css";
@@ -50,41 +57,81 @@ const Gallery = () => {
                     <div className="content">
                         <img src={img4} alt="" />
                     </div>
-                   </div>
-                    <div className='galItem'>
-                        <div className="content">
-                            <img src={img5} alt="" />
-                        </div>
+                </div>
+                <div className='galItem'>
+                    <div className="content">
+                        <img src={img5} alt="" />
                     </div>
-                    <div className='galItem'>
-                        <div className="content">
-                            <img src={img6} alt="" />
-                        </div>
+                </div>
+                <div className='galItem'>
+                    <div className="content">
+                        <img src={img6} alt="" />
                     </div>
-                    <div className='galItem'>
-                        <div className="content">
-                            <img src={img7} alt="" />
-                        </div>
+                </div>
+                <div className='galItem'>
+                    <div className="content">
+                        <img src={img7} alt="" />
                     </div>
-                    <div className='galItem'>
-                        <div className="content">
-                            <img src={img8} alt="" />
-                        </div>
+                </div>
+                <div className='galItem'>
+                    <div className="content">
+                        <img src={img8} alt="" />
                     </div>
+                </div>
 
-                    <div className='galItem'>
-                        <div className="content">
-                            <img src={img9} alt="" />
-                        </div>
-                    </div> <div className='galItem'>
-                        <div className="content">
-                            <img src={img10} alt="" />
-                        </div>
-                    </div> <div className='galItem'>
-                        <div className="content">
-                            <img src={img11} alt="" />
-                        </div>
+                <div className='galItem'>
+                    <div className="content">
+                        <img src={img9} alt="" />
                     </div>
+                </div> <div className='galItem'>
+                    <div className="content">
+                        <img src={img10} alt="" />
+                    </div>
+                </div> <div className='galItem'>
+                    <div className="content">
+                        <img src={img11} alt="" />
+                    </div>
+                </div><div className='galItem'>
+                    <div className="content">
+                        <img src={img12} alt="" />
+                    </div>
+                </div>
+
+                <div className='galItem'>
+                    <div className="content">
+                        <img src={img13} alt="" />
+                    </div>
+                </div>
+
+                <div className='galItem'>
+                    <div className="content">
+                        <img src={img14} alt="" />
+                    </div>
+                </div>
+
+                <div className='galItem'>
+                    <div className="content">
+                        <img src={img15} alt="" />
+                    </div>
+                </div>
+
+                <div className='galItem'>
+                    <div className="content">
+                        <img src={img16} alt="" />
+                    </div>
+                </div>
+
+                <div className='galItem'>
+                    <div className="content">
+                        <img src={img17} alt="" />
+                    </div>
+                </div>
+                <div className='galItem'>
+                    <div className="content">
+                        <img src={img18} alt="" />
+                    </div>
+                </div>
+
 
 
             </Slider>
